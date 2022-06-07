2d019af9f5041cfa32d6b1dbac7713fe1f5d9e69
hshun37 <hshun37@gmail.com>
2022-06-07 09:56:44 +0900

    서브 맵 리뷰 수정

--- api/router/house/location_point copy.php
+++ api/router/house/location_point copy.php
@@ -70,6 +70,45 @@ $sql = "SELECT * FROM location";
 $order = mysqli_query($db, $sql);
 $locate= array();
 
+if (isset($_GET['locate_x'])){
+    $locate_x=$_GET['locate_x'];
+    $locate_y=$_GET['locate_y'];
+    $locate_nx=$_GET['locate_nx'];
+    $locate_ny=$_GET['locate_ny'];
+   $concat= 
+    "SELECT *
+    FROM house,location
+    where location.name IN(concat(`COL 12`,`COL 13`)) and location.location_x >= $locate_x and location.location_x <=$locate_nx and location.location_y >=$locate_y and location.location_y<=$locate_ny
+    order by houseId desc";
+    $c_order= mysqli_query($db,$concat);
+    while($c_row= mysqli_fetch_array($c_order)){
+      array_push($locate, [
+				'x' => $c_row['location_x'], 
+                'y' => $c_row['location_y'],
+                'id' => $c_row['COL 5']
+				]
+			);
+	}
+
+ //아래는 전체 목록 불러오기 센터값 법위 지정 오류로 주석처리 주석풀면 전체 목록을 띄움과 동시에 리스트 부분에는 해당 범위의 목록만 나옴 
+    // while($row= mysqli_fetch_array($order)){
+    //     $c_id=0;
+    //     if ($locate[$c_id]['x']!= $row['location_x']) {
+    //         array_push($locate, [
+	// 			'x' => $row['location_x'], 
+    //             'y' => $row['location_y'],
+    //             'id' => $row['COL 5']
+	// 			]
+	// 		);
+            
+    //     }
+    //     $c_id++;
+
+    // }
+
+
+}
+else{
 while($row= mysqli_fetch_array($order)) {
 	$c_order= mysqli_query($db,$concat);
 	while($c_row= mysqli_fetch_array($c_order)){
@@ -82,6 +121,7 @@ while($row= mysqli_fetch_array($order)) {
 			);
 		}
 	}
+}
 }
 
 
